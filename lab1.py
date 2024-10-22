a3ed3c634fa14a6bed538806b3c1427fa46be3aa
w71431 <mobilonj1258@gmail.com>
2024-10-22 09:04:03 +0200

    dasd

---
+++ lab1.py
@@ -0,0 +1,36 @@
+
+'''# 3
+imie = "Roman"
+kierunek = "Informatyka"
+uczelnia =" WSIZ"
+
+print('Nazywam sie ' + imie,",moj kierunek jest "  + kierunek, ",uczy sie w" + uczelnia )
+
+#4
+#input(prompt)
+
+name = input('Jak ci na imie?')
+print(name)
+print( f"Cześć {name}, witaj na zajęciach")
+
+
+#5
+
+a = int(input("Podaj pierwszy bok prostokata"))
+b = int(input("Podaj drugi bok prostokata"))
+print("Pole twojego prostokata: " + str(a*b))
+print("Obwod twojego prostokata: " + str(a*2+2*b))
+'''
+#6
+
+a = int(input("drogę pokonaną przez samochód"))
+b =  int(input("średnie spalanie (w litrach na 100 km)"))
+
+
+print("m zużyciu paliwa" + str(a/b))
+c = 6.5
+
+
+
+
+
