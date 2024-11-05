2d3591ee2365d24971c5d20492fdfc57f51a5f14
w71431 <mobilonj1258@gmail.com>
2024-11-05 12:49:54 +0100

    lab3

---
+++ lab3/zadanie1.py
@@ -0,0 +1,19 @@
+list = ["Roman","Vlad","Nikita","Maks"]
+
+list.sort()
+print(list)
+
+list.append("Shelby")
+list.append("Thomas")
+print(list)
+
+
+list.insert(2 ,"Dicaprio")
+print(list)
+
+list.pop(-1)
+print(list)
+
+list.reverse()
+list *= 2
+print(list)
